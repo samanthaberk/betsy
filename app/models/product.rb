0c63220cb80fcf79e2a3eb2b49520e57dabc0eef
Kiera <kiera.thomasson@gmail.com>
2018-04-19 15:49:26 -0700

    Added passing  model validation tests. And fixtures to assist.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   # relationship belongs to a user
   validates :name, presence: true
-  validates :name, uniquesness: true
+  validates :name, uniqueness: true
   validates :price, presence: true
   validates :price, numericality: { only_integer: true, greater_than: 0}
 end
