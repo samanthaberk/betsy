f200c49b84ebad589295b61930bcaa2c28db2b07
Samantha Berk <sberk10@gmail.com>
2018-04-19 16:13:14 -0700

    Create controller actions and views for new, create, and show for merchant

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,2 +1,34 @@
 class MerchantsController < ApplicationController
+
+  def index
+    @merchants = Merchant.all
+  end
+
+  def new
+    @merchant = Merchant.new
+  end
+
+  def create
+    merchant = Merchant.new(merchant_params)
+
+    if merchant.save
+      flash[:success] = "Welcome to Sweatsy. Get ready to get sweaty!"
+      redirect_to merchant_path(merchant)
+    else
+      flash[:error] = "Something went wrong!"
+      render :new
+    end
+
+  end
+
+  def show
+    @merchant = Merchant.find_by(id: params[:id])
+  end
+
+  private
+
+  def merchant_params
+    params.require(:merchant).permit(:username, :email)
+  end
+
 end
--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -1,7 +1,48 @@
 require "test_helper"
 
 describe MerchantsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+
+  describe "index" do
+    it "sends a success reponse when there are many merchants" do
+      Merchant.count.must_be :>, 0
+      get merchants_path
+      must_respond_with :success
+    end
+
+    it "sends a success response when there are no merchants" do
+      Merchant.destroy_all
+      Merchant.count.must_equal 0
+      get merchants_path
+      must_respond_with :success
+    end
+  end
+
+  describe "new" do
+    it "sends a success response" do
+      get new_merchant_path
+      must_respond_with :success
+    end
+  end
+
+  describe  "create" do
+    it "can add a valid merchant to the database" do
+      old_merchant_count = Merchant.count
+
+      merchant_data = {
+        username: "test user",
+        email: 'email@gmail.com'
+      }
+
+      merchant = Merchant.new(merchant_data)
+
+      post merchants_path, params: { merchant: merchant_data}
+
+      must_respond_with :redirect
+      must_redirect_to merchant_path(Merchant.last.id)
+
+      Merchant.count.must_equal old_merchant_count + 1
+
+    end
+  end
+
 end
