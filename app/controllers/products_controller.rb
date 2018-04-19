8409cb016b5dc7925f1f156dfa1d41a4f2945af1
Kiera <kiera.thomasson@gmail.com>
2018-04-19 15:59:37 -0700

    skeleton of crud operations.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,37 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @products = Products.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_product
+    @product = products.find_by(id: params[:id])
+
+    head :not_found unless @product
+  end
+
+  def product_params
+    return params.require(:product).permit(:name, :price, :available)
+  end
 end
