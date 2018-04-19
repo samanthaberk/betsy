cd73de7b7d9e9e5370afaf58cb095d3990b9e9fe
Samantha Berk <sberk10@gmail.com>
2018-04-19 13:53:27 -0700

    test validations for merchant

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -1,9 +1,53 @@
 require "test_helper"
 
 describe Merchant do
-  let(:merchant) { Merchant.new }
 
-  it "must be valid" do
-    value(merchant).must_be :valid?
+  describe "validations" do
+
+    before do
+      @merchant = merchants(:merchant_one)
+    end
+
+    it "is valid with all the required fields" do
+      result = @merchant.valid?
+
+      value(result).must_equal true
+    end
+
+    it "is invalid without a username" do
+      @merchant.username = nil
+
+      result = @merchant.valid?
+
+      value(result).must_equal false
+    end
+
+    it "is invalid without an email" do
+      @merchant.email = nil
+
+      result = @merchant.valid?
+
+      value(result).must_equal false
+    end
+
+    it "is invalid when there is a duplicate username" do
+      duplicate = merchants(:merchant_two)
+      duplicate.username = "merchant_one"
+
+      result = duplicate.valid?
+
+      value(result).must_equal false
+    end
+
+    it "is invalid when there is a duplicate email" do
+      duplicate = merchants(:merchant_two)
+      duplicate.email = "merchant_one@gmail.com"
+
+      result = duplicate.valid?
+
+      value(result).must_equal false
+    end
+
   end
+
 end
