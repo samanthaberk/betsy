ad3e0f935ce2f398551f67321bb2ea01d2e3c4ed
Kiera <kiera.thomasson@gmail.com>
2018-04-19 16:38:43 -0700

    Added resources product routes. Updated products controller with basic functionality.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,17 +10,32 @@ class ProductsController < ApplicationController
   end
 
   def create
+    @product = Product.new(product_params)
+
+    if product.save
+      flash[:success] = "Product added successfully"
+      redirect_to products_path
+    else
+      flash.now[:failure] = "Validations Failed"
+      render :new, :status :bad_request
   end
 
-  def edit
+  def edit; end
 
-  end
+  def show; end
 
-  def show
+  def update
+    @product.assign_attributes(product_params)
 
+    if @product.save
+      redirect_to product_path
+    else render :edit, status: :bad_request
   end
 
   def destroy
+    @product.destroy
+
+    redirect_to products_path
   end
 
   private
