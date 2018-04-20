d2165cd3e47170d8dee19c57eb69c8b2cd5fa3e4
Kiera <kiera.thomasson@gmail.com>
2018-04-20 14:31:46 -0700

    Added column to order_products table.

---
+++ db/migrate/20180420212840_add_quantity_field_to_order_products_table.rb
@@ -0,0 +1,5 @@
+class AddQuantityFieldToOrderProductsTable < ActiveRecord::Migration[5.1]
+  def change
+    add_column :orders_products, :quantity, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180420210859) do
+ActiveRecord::Schema.define(version: 20180420212840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20180420210859) do
   create_table "orders_products", force: :cascade do |t|
     t.bigint "order_id"
     t.bigint "product_id"
+    t.integer "quantity"
     t.index ["order_id"], name: "index_orders_products_on_order_id"
     t.index ["product_id"], name: "index_orders_products_on_product_id"
   end
