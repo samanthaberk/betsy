a668de75b73486b59ff1e7d39fa275c4a08fbe43
Samantha Berk <sberk10@gmail.com>
2018-04-22 15:53:26 -0700

    fix relations for reviews and product_categories for Merchant model and run tests

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,8 +1,8 @@
 class Merchant < ApplicationRecord
   has_many :products
   has_many :orders
-  has_many :reviewed_products, through: :reviews, source: :product
-  has_many :product_categories, through: :categories, source: :product
+  has_many :reviews, through: :products, source: :order_products
+  has_many :product_categories, through: :products, source: :categories
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
