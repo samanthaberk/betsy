d6e4dc9b9937c20ee63892f89c67946372b51503
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-04-22 11:43:53 -0700

    Fixed seed data so prices are in cents

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,5 +7,5 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :price, presence: true
-  validates :price, numericality: { greater_than: 0}
+  validates :price, numericality: { only_integer: true, greater_than: 0}
 end
