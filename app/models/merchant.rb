bcbda2cb655880810f8ae2b406b0e8c22aba75cc
Samantha Berk <sberk10@gmail.com>
2018-04-22 16:54:39 -0700

    Update merchants index page

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -7,13 +7,4 @@ class Merchant < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
 
-  def find_average_rating(merchant)
-    total_rating = 0
-    merchant.reviews.inject do | review, rating |
-      review.rating
-    end
-    [1,2,3,4].inject(0) {|res, x| x * 2 + res}
-
-  end
-
 end
