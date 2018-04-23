a5d8ade1e461b78fb7d4fe761a9e865229c2d5ad
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-04-23 16:55:13 -0700

    Generate carts controller

--- app/controllers/order_products_controller.rb
+++ app/controllers/order_products_controller.rb
@@ -1,5 +1,16 @@
 class OrderProductsController < ApplicationController
 
+  def create
+    @order = current_order
+    @item = @order.order_products.new(item_params)
+    @order.save
+    session[:order_id] = @order.id
+    redirect_to products_path
+  end
 
+  private
+  def item_params
+    params.require(:order_product).permit(:quantity, :product_id)
+  end
 
 end
