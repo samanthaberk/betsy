41195bac698c98bb14b7f21298561fd08fad6b59
Maja Graonic <m.graonic@gmail.com>
2018-04-23 11:36:06 -0700

    completed review model tests

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   validates :rating, presence: true
+  validates_numericality_of :rating, only_integer: true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 5
   validates :description, presence: true
   belongs_to :product
-
 end
