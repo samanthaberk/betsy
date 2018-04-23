aa8aa0020544050fe7c31c94a63e39a30f78a4c9
Maja Graonic <m.graonic@gmail.com>
2018-04-23 10:49:47 -0700

    transferred review calculation logic to prodct model

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,18 +3,4 @@ class Review < ApplicationRecord
   validates :description, presence: true
   belongs_to :product
 
-  def total
-    self.count
-  end
-
-  def average
-    reviews = Review.all
-    review_count = reviews.count
-
-    ratings = reviews.map { |review| review.rating}
-    ratings_sum = ratings.inject { |sum, rating| sum + rating }
-
-    return ratings_sum / review_count
-  end
-
 end
