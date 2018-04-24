469c5342683c5b8ef77ef0130f0f76cf10123ebd
Samantha Berk <sberk10@gmail.com>
2018-04-24 12:05:46 -0700

    Replace end in routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,28 +6,29 @@ Rails.application.routes.draw do
   post '/products/:id/add_review', to: 'products#create_review', as: :create_review
   resources :products, except: [:edit, :update] do
 
-  get '/auth/github', as: 'github_login'
+    get '/auth/github', as: 'github_login'
 
-  post '/logout', to: 'sessions#logout', as: 'logout'
-  get "/auth/:provider/callback", to: "sessions#create", as: 'auth_callback'
+    post '/logout', to: 'sessions#logout', as: 'logout'
+    get "/auth/:provider/callback", to: "sessions#create", as: 'auth_callback'
 
-  resources :products do
-    resources :reviews, only: [:new, :show, :create]
+    resources :products do
+      resources :reviews, only: [:new, :show, :create]
+    end
+
+    resources :orders, only: [:show, :new, :create]
+
+    # may not need all routes for order_products
+    resources :order_products
+    resource :cart, only: [:show]
+
+    resources :categories, only: [:new, :create, :index, :show] do
+      resources :product, only: [:root]
+    end
+
+    resources :merchants, except: [:edit, :update, :destroy] do
+      resources :products, only: [:index]
+      resources :orders, only: [:index]
+    end
+
   end
-
-  resources :orders, only: [:show, :new, :create]
-
-  # may not need all routes for order_products
-  resources :order_products
-  resource :cart, only: [:show]
-
-  resources :categories, only: [:new, :create, :index, :show] do
-    resources :product, only: [:root]
-  end
-
-  resources :merchants, except: [:edit, :update, :destroy] do
-    resources :products, only: [:index]
-    resources :orders, only: [:index]
-  end
-
 end
