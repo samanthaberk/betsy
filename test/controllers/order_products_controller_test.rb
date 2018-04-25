d3c23e604cc8a9b6a848bdce742573d65ab31fdd
Samantha Berk <sberk10@gmail.com>
2018-04-24 21:08:59 -0700

    three tests for order_products controller

--- test/controllers/order_products_controller_test.rb
+++ test/controllers/order_products_controller_test.rb
@@ -1,7 +1,61 @@
 require "test_helper"
-
+require 'pry'
 describe OrderProductsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+
+  describe 'create' do
+
+    it "can create an OrderProduct with valid data" do
+      order = Order.first
+
+      item_data = {
+        order_id: order.id,
+        quantity: 10,
+        product_id: Product.first.id
+      }
+      # equals 0
+      old_count = order.order_products.count
+
+      post order_products_path, params: { order_product: item_data }
+
+      must_respond_with :redirect
+      must_redirect_to products_path
+      # still equals 0, not saving
+      order.order_products.count.must_equal old_count + 1
+    end
+
+    it "does not create an OrderProduct with incomplete data" do
+      order = Order.first
+
+      item_data = {
+        quantity: 10,
+      }
+      old_count = order.order_products.count
+
+      post order_products_path, params: { order_product: item_data }
+
+      must_respond_with :bad_request
+      # still equals 0, not saving
+      order.order_products.count.must_equal old_count
+    end
+
+  end
+
+  describe 'update' do
+
+    it "updates the quantity of an existing OrderProduct with a valid number" do
+      order = Order.first
+
+      order_product = OrderProduct.create!(quantity: 1, order_id: order.id, product_id: Product.first.id)
+
+      item_data = order_product.attributes
+      item_data[:quantity] = 2
+
+      order_product.assign_attributes(item_data)
+
+      patch order_product_path(order_product), params: { order_product: item_data }
+
+    end
+
+  end
+
 end
