1986d41876922940db21041ca2bdad99b600da8b
Samantha Berk <sberk10@gmail.com>
2018-04-26 15:32:21 -0700

    index tests for categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 describe CategoriesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+
+  describe 'index' do
+
+    it "sends a success response when there are many categories" do
+      Category.count.must_be :>, 0
+
+      get categories_path
+
+      must_respond_with :success
+    end
+
+    it "sends a success response when there are no categories" do
+      Category.delete_all
+      
+      Category.count.must_equal 0
+
+      get categories_path
+
+      must_respond_with :success
+    end
+
+  end
+
 end
