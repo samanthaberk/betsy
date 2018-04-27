456e30923afb0260a773df22e2696033220886f7
Maja Graonic <m.graonic@gmail.com>
2018-04-27 11:46:15 -0700

    category controller tests passing; minor change to category save redirect

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -19,4 +19,21 @@ describe CategoriesController do
     end
   end
 
+  describe "create" do
+    it "saves a valid category" do
+      category_data = {name: 'new category'}
+      merchant = Merchant.first
+      old_category_count = Category.count
+
+      Category.new(category_data).must_be :valid?
+      login(merchant)
+
+      post categories_path, params: {category: category_data}
+
+      must_respond_with :redirect
+      must_redirect_to merchant_path(id: merchant.id)
+      Category.count.must_equal old_category_count + 1
+    end
+  end
+
 end
